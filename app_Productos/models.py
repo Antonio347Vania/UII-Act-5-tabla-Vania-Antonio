14298f7d2dd8bfb836056a71c441d536c2438075
SilvaLizet <a.23308051280376@cbtis128.edu.mx>
2025-10-07 12:55:20 -0600

    version 2

--- app_Productos/models.py
+++ app_Productos/models.py
@@ -7,5 +7,6 @@ class Productos(models.Model):
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
 
+
     def __str__(self):
         return f"{self.nombre} - ${self.precio}"
--- app_Productos/views.py
+++ app_Productos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from. models import Productos
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    productos = Productos.objects.all()
+    return render(request, 'index.html', {'productos': productos})
